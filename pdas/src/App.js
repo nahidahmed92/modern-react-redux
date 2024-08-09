7bd3ef95033e1cceef621241cd0300af1b309c1e
Nahid Ahmed <npahmed92@gmail.com>
2024-08-09 13:19:29 -0400

    Section 3 - props handling

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,12 +1,15 @@
 import ProfileCard from './ProfileCard';
+import alexa from './images/alexa.png';
+import cortana from './images/cortana.png';
+import siri from './images/siri.png';
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title="Alexa" twitter="@alexa99" image={alexa} />
+      <ProfileCard title="Cortana" twitter="@cortana32" image={cortana} />
+      <ProfileCard title="Siri" twitter="@siri01" image={siri} />
     </div>
   );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,5 +1,15 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+function ProfileCard({ title, twitter, image }) {
+  // function ProfileCard(props) {
+  // const title = props.title;
+  // const twitter = props.twitter;
+  // const { title, twitter } = props;
+  return (
+    <div>
+      <img src={image} />
+      <div>{title}</div>
+      <div>{twitter}</div>
+    </div>
+  );
 }
 
 export default ProfileCard;
