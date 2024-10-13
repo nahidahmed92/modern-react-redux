e7cae2bd6e85b7310b6b610cb350f305ca56507f
Nahid Ahmed <npahmed92@gmail.com>
2024-10-13 15:51:18 -0400

    Section 3: Building with Reusable Components

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -3,13 +3,46 @@ import alexa from './images/alexa.png';
 import cortana from './images/cortana.png';
 import siri from './images/siri.png';
 
+import 'bulma/css/bulma.css';
+
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" twitter="@alexa99" image={alexa} />
-      <ProfileCard title="Cortana" twitter="@cortana32" image={cortana} />
-      <ProfileCard title="Siri" twitter="@siri01" image={siri} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <div className="sections">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                twitter="@alexa99"
+                image={alexa}
+                description="Alexa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                twitter="@cortana32"
+                image={cortana}
+                description="Cortana was made by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                twitter="@siri01"
+                image={siri}
+                description="Siri was made by Apple and is bring phased out"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,13 +1,30 @@
-function ProfileCard({ title, twitter, image }) {
+function ProfileCard({ title, twitter, image, description }) {
   // function ProfileCard(props) {
   // const title = props.title;
   // const twitter = props.twitter;
   // const { title, twitter } = props;
   return (
-    <div>
-      <img src={image} />
-      <div>{title}</div>
-      <div>{twitter}</div>
+    // <div>
+    //   <img src={image} alt={title + ' ' + 'logo'} />
+    //   <div>{title}</div>
+    //   <div>{twitter}</div>
+    // </div>
+
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt={title + ' ' + 'logo'} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{twitter}</p>
+          </div>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
